e85b9a4a0266e258bf913e6122cacc50c288d3c3
dima_tolshin <dima_tolshin@mail.ru>
2024-07-12 01:06:04 +0300

    commit

--- main/signals.py
+++ main/signals.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Person
@@ -6,7 +7,7 @@ from .tasks import energy_task
 
 @receiver(post_save, sender=Person)
 def my_changed(sender, instance, created, **kwargs):
-    update_fields = kwargs.get('update_fields')
-    print(update_fields)
-    if update_fields is not None and 'now_energy' in update_fields and instance.now_energy != instance.start_energy:
+    task_id = f'energy_task_{instance.id}'
+    if not cache.get(task_id):
+        cache.set(task_id, 'in_progress', timeout=600 * 5)
         energy_task.apply_async((instance.id,), countdown=5)
