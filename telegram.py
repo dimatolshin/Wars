41be0f54b46c829945ba13dea3f098713f118434
dima_tolshin <dima_tolshin@mail.ru>
2024-07-15 19:54:28 +0300

    commit

--- telegram.py
+++ telegram.py
@@ -1,4 +1,47 @@
-from aiogram import Bot, Dispatcher, executor, types
+import asyncio
+import logging
+import sys
+from os import getenv
 
-bot = Bot('7474449523:AAFLTVA2qqS15MaI_PBW7kX3gfoxoKJGKNQ')
-dp = Dispatcher(bot)
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message, InlineKeyboardButton, WebAppInfo, InlineKeyboardMarkup
+
+# Bot token can be obtained via https://t.me/BotFather
+TOKEN = '7474449523:AAFLTVA2qqS15MaI_PBW7kX3gfoxoKJGKNQ'
+
+# All handlers should be attached to the Router (or Dispatcher)
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    markup = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text='Играть',
+                    web_app=WebAppInfo(url=f'https://docs.aiogram.dev/en/latest/'),
+                )
+            ]
+        ]
+    )
+    await message.answer(f"Супер игра, попробуй свои силы!", reply_markup=markup)
+
+
+
+
+async def main() -> None:
+    # Initialize Bot instance with default bot properties which will be passed to all API calls
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
