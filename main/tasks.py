cf65c476229fd65da50c56d1a068ed1521944163
dima_tolshin <dima_tolshin@mail.ru>
2024-07-23 17:03:33 +0300

    fix

--- main/tasks.py
+++ main/tasks.py
@@ -8,12 +8,12 @@ def energy_task(instance_id):
     task_id = f'energy_task_{instance_id}'
     instance = Person.objects.get(id=instance_id)
     if instance.now_energy < instance.start_energy:
-        instance.now_energy += 3
+        instance.now_energy += 1
         if instance.now_energy >=instance.start_energy:
             instance.now_energy = instance.start_energy
             instance.save(update_fields=['now_energy', ])
             cache.delete(task_id)
         instance.save(update_fields=['now_energy',])
-        energy_task.apply_async((instance.id,), countdown=5)
+        energy_task.apply_async((instance.id,), countdown=1)
     else:
         cache.delete(task_id)
