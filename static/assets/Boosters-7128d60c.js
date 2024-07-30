e592a8fe94ff71b1e04428269125193a64177fe7
unknown <UstinovichSergey>
2024-07-30 17:37:38 +0300

    fixLink

---
+++ static/assets/Boosters-7128d60c.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-6f548cf9.js";import{M as t}from"./ModalRoute-1509d53e.js";const e="_tes_xu9a1_1",o={tes:e};function n(){return s.jsx(t,{children:s.jsx("div",{className:o.tes,children:"Boosters"})})}export{n as default};
---
+++ static/assets/Leaders-327ba09d.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-6f548cf9.js";import{M as e}from"./ModalRoute-1509d53e.js";const t="_tes_xu9a1_1",r={tes:t};function n(){return s.jsx(e,{children:s.jsx("div",{className:r.tes,children:"Leaders"})})}export{n as default};
