bf0803fafef30327adac2c9914d579227d3d91c1
maksimjoyharper <makksss1123@icloud.com>
2024-10-09 12:24:43 +0300

    feat: tasks page

---
+++ static/assets/Leaders-115b76ee.js
@@ -0,0 +1,1 @@
+import{j as s,M as e}from"./index-91530331.js";const t="_tes_xu9a1_1",a={tes:t};function o(){return s.jsx(e,{children:s.jsx("div",{className:a.tes,children:"Leaders"})})}export{o as default};
---
+++ static/assets/Passive-1e8a4452.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-91530331.js";const r=()=>s.jsx("div",{children:"Passive"});export{r as default};
