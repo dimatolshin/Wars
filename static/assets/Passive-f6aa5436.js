6a02f1e0791933936bb46e4c7d05154a8349a2dc
maksimjoyharper <makksss1123@icloud.com>
2024-10-11 19:32:12 +0300

    feat: fix bugs styles

---
+++ static/assets/Passive-f6aa5436.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-e914329c.js";const r=()=>s.jsx("div",{children:"Passive"});export{r as default};
--- static/assets/Special-b414696c.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-7385f0ba.js";const t=()=>e.jsx("div",{children:"Special"});export{t as default};
